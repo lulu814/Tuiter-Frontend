4517e965e3f50821c285f72ca49426586eea544c
Chengcheng Liu <liu.cheng@northeastern.edu>
2022-04-20 10:55:34 -0400

    feat: Code format - format the code - remove all test code

--- src/components/home/image-uploader.js
+++ src/components/home/image-uploader.js
@@ -1,77 +1,86 @@
-import React,{useState} from "react";
-// import OverlayTrigger from "react-bootstrap/esm/OverlayTrigger";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 import ImageUploading from "react-images-uploading";
-import {Modal,Button} from "react-bootstrap";
-import axios from "axios";
-import {Image} from "cloudinary-react";
+import { Modal, Button } from "react-bootstrap";
 
-const upload = ({images, setImages, setImageIds}) => {
-    const maxNumber = 4;
-    const onChange = (imageList, addUpdateIndex) => {
+const upload = ({ images, setImages, setImageIds }) => {
+  const maxNumber = 4;
+  const onChange = (imageList, addUpdateIndex) => {
     // data for submit
     setImages(imageList);
-    };
+  };
 
-    return (
-        <div>
-            <ImageUploading
-                multiple
-                value={images}
-                onChange={onChange}
-                maxNumber={maxNumber}
-                dataURLKey="data_url"
-            >
-                {({
-                    imageList,
-                    onImageUpload,
-                    onImageRemoveAll,
-                    onImageUpdate,
-                    onImageRemove,
-                    isDragging,
-                    dragProps
-                }) => (
-                // preview
+  return (
+    <div>
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // preview
           <div className="row upload__image-wrapper">
             <button
-              className="col btn btn-outline-info btn-sm" 
+              className="col btn btn-outline-info btn-sm"
               style={isDragging ? { color: "red" } : null}
               onClick={onImageUpload}
               {...dragProps}
             >
               Choose Images
             </button>
-            <button className="col btn btn-outline-danger btn-sm" onClick={onImageRemoveAll}>Remove all</button>
+            <button
+              className="col btn btn-outline-danger btn-sm"
+              onClick={onImageRemoveAll}
+            >
+              Remove all
+            </button>
             {/*<button className="col btn btn-outline-success btn-md" onClick={uploadImage}>Upload Image</button>*/}
 
             <div className="row">
-                {imageList.map((image, index) => (
+              {imageList.map((image, index) => (
                 <div key={index} className="col image-item">
-                    <img className="col" src={image.data_url} alt="" width="150" />
-                    <div className="col">
-                    <i className="far fa-upload me-3" onClick={() => onImageUpdate(index)}/>
-                    <i className="far fa-trash me-3" onClick={() => onImageRemove(index)}/>
-                    </div>
+                  <img
+                    className="col"
+                    src={image.data_url}
+                    alt=""
+                    width="150"
+                  />
+                  <div className="col">
+                    <i
+                      className="far fa-upload me-3"
+                      onClick={() => onImageUpdate(index)}
+                    />
+                    <i
+                      className="far fa-trash me-3"
+                      onClick={() => onImageRemove(index)}
+                    />
+                  </div>
                 </div>
-                ))}                
+              ))}
             </div>
-
           </div>
-                )}
-        </ImageUploading>
-        </div>
+        )}
+      </ImageUploading>
+    </div>
+  );
+};
 
-    )
-}
-
-const Example = ({images, setImages, setImageIds}) => {
+const Example = ({ images, setImages, setImageIds }) => {
   const [lgShow, setLgShow] = useState(false);
   const handleClose = () => setLgShow(false);
   const handleShow = () => setLgShow(true);
   return (
     <>
-      <i className="fas fa-image me-3 image-uploading" onClick={handleShow}>
-      </i>
+      <i className="fas fa-image me-3 image-uploading" onClick={handleShow}></i>
 
       <Modal
         size="lg"
@@ -82,9 +91,7 @@ const Example = ({images, setImages, setImageIds}) => {
         <Modal.Header closeButton>
           <Modal.Title>Upload Images</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-            {upload({images, setImages, setImageIds})}
-        </Modal.Body>
+        <Modal.Body>{upload({ images, setImages, setImageIds })}</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
@@ -96,6 +103,6 @@ const Example = ({images, setImages, setImageIds}) => {
       </Modal>
     </>
   );
-}
+};
 
 export default Example;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,123 +1,121 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Tuits from "../tuits/index";
 import * as service from "../../services/tuits-service";
-import * as security_service from "../../services/security-service"
+import * as security_service from "../../services/security-service";
 import EmojiPicker from "./emoji-picker";
 import ImageUploader from "./image-uploader";
-import {useLocation, useParams} from "react-router-dom";
-
-//cloudary
-import Cloudinary from "./cloudinary";
+import { useLocation, useParams } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
-    const location = useLocation();
-    const {uid} = useParams();
-    const [isLoggedIn, setLoggedIn] = useState(false);
-    const [tuits, setTuits] = useState([]);
-    const [tuit, setTuit] = useState('');
-    //set image state
-    const [images, setImages] = useState([]);
-    const [imageSelected, setImageSelected] = useState('');
-    const [imageIds, setImageIds] = useState([]);
+  const location = useLocation();
+  const { uid } = useParams();
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [tuits, setTuits] = useState([]);
+  const [tuit, setTuit] = useState("");
+  //set image state
+  const [images, setImages] = useState([]);
+  const [imageIds, setImageIds] = useState([]);
 
-    const userId = uid;
-    const isUserLoggedIn = () =>
-        security_service.profile()
-            .then((user) => {
-                if (user) {
-                    setLoggedIn(true);
-                } else {
-                    setLoggedIn(false);
-                }
-            })
-    const findTuits = () =>
-        service.findAllTuits()
-            .then(tuits => setTuits(tuits));
-    useEffect(isUserLoggedIn);
-    useEffect(() => {
-        let isMounted = true;
-        findTuits()
-        return () => {
-            isMounted = false;
-        }
-    }, []);
-
-    const uploadImage = () => {
-        const formData = new FormData();
-        images.forEach(f =>  formData.append("images", f.file))
-        formData.append("enctype", "multipart/form-data")
-        return axios.post(
-            "http://localhost:4000/api/tuits/image/upload",
-            formData,{
-                headers: {
-                    'content-type': 'multipart/form-data'
-                }
-            }).then((response) => {
-            const imageIds = response.data.map(res => {
-                return res.public_id
-            })
-            setImageIds(imageIds);
-            let draftTuit = {tuit: tuit};
-            draftTuit['image'] = imageIds;
-            service.createTuit('me', draftTuit)
-                .then(findTuits)
-        });
+  const userId = uid;
+  const isUserLoggedIn = () =>
+    security_service.profile().then((user) => {
+      if (user) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    });
+  const findTuits = () =>
+    service.findAllTuits().then((tuits) => setTuits(tuits));
+  useEffect(isUserLoggedIn);
+  useEffect(() => {
+    let isMounted = true;
+    findTuits();
+    return () => {
+      isMounted = false;
     };
+  }, []);
 
-    const createTuit = () => {
-        if (images.length > 0) {
-            uploadImage();
-        } else {
-            service.createTuit('me', {tuit})
-                .then(findTuits)
-        }
+  const uploadImage = () => {
+    const formData = new FormData();
+    images.forEach((f) => formData.append("images", f.file));
+    formData.append("enctype", "multipart/form-data");
+    return axios
+      .post("http://localhost:4000/api/tuits/image/upload", formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        const imageIds = response.data.map((res) => {
+          return res.public_id;
+        });
+        setImageIds(imageIds);
+        let draftTuit = { tuit: tuit };
+        draftTuit["image"] = imageIds;
+        service.createTuit("me", draftTuit).then(findTuits);
+      });
+  };
 
+  const createTuit = () => {
+    if (images.length > 0) {
+      uploadImage();
+    } else {
+      service.createTuit("me", { tuit }).then(findTuits);
     }
+  };
 
-    return (
-        <div className="ttr-home">
-            <div className="border border-bottom-0">
-                <h4 className="fw-bold p-2">Home Screen</h4>
-                <div className="d-flex">
-                    <div className="p-2">
-                        <img className="ttr-width-50px rounded-circle"
-                             src="../images/nasa-logo.jpg"/>
-                    </div>
-                    <div className="p-2 w-100">
+  return (
+    <div className="ttr-home">
+      <div className="border border-bottom-0">
+        <h4 className="fw-bold p-2">Home Screen</h4>
+        <div className="d-flex">
+          <div className="p-2">
+            <img
+              className="ttr-width-50px rounded-circle"
+              src="../images/nasa-logo.jpg"
+            />
+          </div>
+          <div className="p-2 w-100">
             <textarea
-                style={{fontFamily: "twemoji"}}
-                value={tuit}
-                onChange={(e) =>
-                    setTuit(e.target.value)}
-                placeholder="What's happening?"
-                className="w-100 border-0"/>
-                        <div className="row">
-                            <div className="col-10 ttr-font-size-150pc text-primary">
-                                <ImageUploader images={images} setImages={setImages}  setImageIds={setImageIds}/>
-                                <i className="far fa-video me-3"/>
-                                <i className="far fa-bar-chart me-3"/>
-                                <EmojiPicker tuit={tuit} setTuit={setTuit}/>
-                                <i className="far fa-calendar me-3"/>
-                                <i className="far fa-map-location me-3"/>
-                            </div>
-                            <div className="col-2">
-                                {isLoggedIn && <a onClick={createTuit}
-                                                  className={`btn btn-primary rounded-pill fa-pull-right
-                                fw-bold ps-4 pe-4`}>
-                                    Tuit
-                                </a>}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              style={{ fontFamily: "twemoji" }}
+              value={tuit}
+              onChange={(e) => setTuit(e.target.value)}
+              placeholder="What's happening?"
+              className="w-100 border-0"
+            />
+            <div className="row">
+              <div className="col-10 ttr-font-size-150pc text-primary">
+                <ImageUploader
+                  images={images}
+                  setImages={setImages}
+                  setImageIds={setImageIds}
+                />
+                <i className="far fa-video me-3" />
+                <i className="far fa-bar-chart me-3" />
+                <EmojiPicker tuit={tuit} setTuit={setTuit} />
+                <i className="far fa-calendar me-3" />
+                <i className="far fa-map-location me-3" />
+              </div>
+              <div className="col-2">
+                {isLoggedIn && (
+                  <a
+                    onClick={createTuit}
+                    className={`btn btn-primary rounded-pill fa-pull-right
+                                fw-bold ps-4 pe-4`}
+                  >
+                    Tuit
+                  </a>
+                )}
+              </div>
             </div>
-            {isLoggedIn && <Tuits tuits={tuits}
-                                  refreshTuits={findTuits}/>}
-        <div>
-            <Cloudinary imageSelected={imageSelected} setImageSelected={setImageSelected}/>
+          </div>
         </div>
-        </div>
-    );
+      </div>
+      {isLoggedIn && <Tuits tuits={tuits} refreshTuits={findTuits} />}
+      <div></div>
+    </div>
+  );
 };
 export default Home;
