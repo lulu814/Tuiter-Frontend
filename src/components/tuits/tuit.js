17002fb5c6e2dc899031d5465d0751ac440550aa
Lu Liu <luliu@Lus-MacBook-Pro.local>
2022-04-21 12:15:22 -0400

    reformat code and re-add emoji

--- src/components/tuits/tuit.js
+++ src/components/tuits/tuit.js
@@ -4,7 +4,7 @@ import emoji from 'react-easy-emoji'
 import TuitStats from "./tuit-stats";
 import TuitImage from "./tuit-image";
 import TuitVideo from "./tuit-video";
-import {Link, useNavigate} from "react-router-dom";
+// import {Link, useNavigate} from "react-router-dom";
 
 
 /**
@@ -17,7 +17,7 @@ import {Link, useNavigate} from "react-router-dom";
  * @constructor
  */
 const Tuit = ({tuit, deleteTuit, likeTuit, dislikeTuit, bookmarkTuit}) => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const daysOld = (tuit) => {
         const now = new Date();
         const nowMillis = now.getTime();
@@ -25,53 +25,53 @@ const Tuit = ({tuit, deleteTuit, likeTuit, dislikeTuit, bookmarkTuit}) => {
         const postedMillis = posted.getTime();
         const oldMillis = nowMillis - postedMillis;
         let old = 0.0;
-        const secondsOld = oldMillis/1000.0;
-        const minutesOld = secondsOld/60.0;
-        const hoursOld = minutesOld/60.0;
-        const daysOld = hoursOld/24.0;
-        if(daysOld > 1) {
+        const secondsOld = oldMillis / 1000.0;
+        const minutesOld = secondsOld / 60.0;
+        const hoursOld = minutesOld / 60.0;
+        const daysOld = hoursOld / 24.0;
+        if (daysOld > 1) {
             old = Math.round(daysOld) + 'd';
-        } else if(hoursOld > 1) {
+        } else if (hoursOld > 1) {
             old = Math.round(hoursOld) + 'h';
-        } else if(minutesOld > 1) {
+        } else if (minutesOld > 1) {
             old = Math.round(minutesOld) + 'm';
-        } else if(secondsOld > 1) {
+        } else if (secondsOld > 1) {
             old = Math.round(secondsOld) + 's';
         }
         return old;
     }
-  return(
-    // <li onClick={() => navigate(`/tuit/${tuit._id}`)}
-    <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
-      <div className="pe-2">
-        {
-          tuit.postedBy &&
-          <img src={`../images/${tuit.postedBy.username}.jpg`}
-               className="ttr-tuit-avatar-logo rounded-circle"/>
-        }
-      </div>
-      <div className="w-100">
-          <i onClick={() => deleteTuit(tuit._id)} className="fas fa-remove fa-2x fa-pull-right"/>
-          {/*<Link to={`/tuit/${tuit._id}`}>*/}
-          {/*<i className="float-end fas fa-circle-ellipsis me-1"/>*/}
-          {/*</Link>*/}
-        <h2
-          className="fs-5">
-          {tuit.postedBy && tuit.postedBy.username}
-          @{tuit.postedBy && tuit.postedBy.username} -
-            <span className="ms-1">{daysOld(tuit)}</span></h2>
-        {tuit.tuit}
-        {
-          tuit.youtube &&
-            <TuitVideo tuit={tuit}/>
-        }
-        {
-          tuit.image &&
-          <TuitImage tuit={tuit}/>
-        }
-        <TuitStats tuit={tuit} likeTuit={likeTuit} dislikeTuit={dislikeTuit} bookmarkTuit={bookmarkTuit}/>
-      </div>
-    </li>
-  );
+    return (
+        // <li onClick={() => navigate(`/tuit/${tuit._id}`)}
+        <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
+            <div className="pe-2">
+                {
+                    tuit.postedBy &&
+                    <img src={`../images/${tuit.postedBy.username}.jpg`}
+                         className="ttr-tuit-avatar-logo rounded-circle"/>
+                }
+            </div>
+            <div className="w-100">
+                <i onClick={() => deleteTuit(tuit._id)} className="fas fa-remove fa-2x fa-pull-right"/>
+                {/*<Link to={`/tuit/${tuit._id}`}>*/}
+                {/*<i className="float-end fas fa-circle-ellipsis me-1"/>*/}
+                {/*</Link>*/}
+                <h2
+                    className="fs-5">
+                    {tuit.postedBy && tuit.postedBy.username}
+                    @{tuit.postedBy && tuit.postedBy.username} -
+                    <span className="ms-1">{daysOld(tuit)}</span></h2>
+                {emoji(tuit.tuit)}
+                {
+                    tuit.youtube &&
+                    <TuitVideo tuit={tuit}/>
+                }
+                {
+                    tuit.image &&
+                    <TuitImage tuit={tuit}/>
+                }
+                <TuitStats tuit={tuit} likeTuit={likeTuit} dislikeTuit={dislikeTuit} bookmarkTuit={bookmarkTuit}/>
+            </div>
+        </li>
+    );
 }
 export default Tuit;
